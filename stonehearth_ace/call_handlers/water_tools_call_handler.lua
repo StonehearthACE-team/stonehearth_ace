91eb3b47352085fc77b9bf4cf7790b5fec64f26a
paulthegreat <paulthegreat@users.noreply.github.com>
2018-07-15 10:34:33 -0500

    updated water gate to switch between different commands
    Allows easily setting different text and icons for the commands.

--- stonehearth_ace/call_handlers/water_tools_call_handler.lua
+++ stonehearth_ace/call_handlers/water_tools_call_handler.lua
@@ -3,10 +3,11 @@ local log = radiant.log.create_logger('water_tools')
 
 local WaterToolsCallHandler = class()
 
-function WaterToolsCallHandler:toggle_water_gate_command(session, response, water_gate)
-	validator.expect_argument_types({'Entity'}, water_gate)
+function WaterToolsCallHandler:toggle_water_gate_command(session, response, water_gate, old_command, new_command)
+	validator.expect_argument_types({'Entity', 'string', 'string'}, water_gate, old_command, new_command)
 	validator.expect.matching_player_id(session.player_id, water_gate)
 	
+	-- original enabled status would be doing true == 1, but we want to negate it because we're toggling it
 	local enabled = radiant.entities.get_attribute(water_gate, 'enabled', 0) == 0
 	local region_collision_shape = water_gate:get_component('region_collision_shape')
 	local new_collision_type
@@ -20,7 +21,14 @@ function WaterToolsCallHandler:toggle_water_gate_command(session, response, wate
 		radiant.entities.set_attribute(water_gate, 'enabled', enabled and 1 or 0)
 		region_collision_shape:set_region_collision_type(new_collision_type)
 
-		-- do anything else here like playing animations or messing with the commands/icons
+		-- swap commands
+		local commands_component = water_gate:get_component('stonehearth:commands')
+		if commands_component then
+			commands_component:remove_command(old_command)
+			commands_component:add_command(new_command)
+		end
+
+		-- do anything else here like playing animations?
 
 		radiant.events.trigger(radiant, 'stonehearth_ace:on_water_gate_toggled', { water_gate = water_gate, enabled = enabled })
 		return true
