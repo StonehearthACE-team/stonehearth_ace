a0e9dd9a85a307dbe32af9b40c319349a6574df8
paulthegreat <paulthegreat@users.noreply.github.com>
2018-07-15 23:39:21 -0500

    improved water tools
    Added component for water tools, used by water_gate and the new water_pump, simplifying commands/call handler.

--- stonehearth_ace/call_handlers/water_tools_call_handler.lua
+++ stonehearth_ace/call_handlers/water_tools_call_handler.lua
@@ -3,38 +3,18 @@ local log = radiant.log.create_logger('water_tools')
 
 local WaterToolsCallHandler = class()
 
-function WaterToolsCallHandler:toggle_water_gate_command(session, response, water_gate, old_command, new_command)
-	validator.expect_argument_types({'Entity', 'string', 'string'}, water_gate, old_command, new_command)
-	validator.expect.matching_player_id(session.player_id, water_gate)
+function WaterToolsCallHandler:toggle_water_tools_command(session, response, entity, enabled)
+	validator.expect_argument_types({'Entity'}, entity)
+	validator.expect.matching_player_id(session.player_id, entity)
 	
-	-- original enabled status would be doing true == 1, but we want to negate it because we're toggling it
-	local enabled = radiant.entities.get_attribute(water_gate, 'enabled', 0) == 0
-	local region_collision_shape = water_gate:get_component('region_collision_shape')
-	local new_collision_type
-	if enabled then
-		new_collision_type = _radiant.om.RegionCollisionShape.PLATFORM
-	else
-		new_collision_type = _radiant.om.RegionCollisionShape.SOLID
-	end
-
-	if region_collision_shape then
-		radiant.entities.set_attribute(water_gate, 'enabled', enabled and 1 or 0)
-		region_collision_shape:set_region_collision_type(new_collision_type)
-
-		-- swap commands
-		local commands_component = water_gate:get_component('stonehearth:commands')
-		if commands_component then
-			commands_component:remove_command(old_command)
-			commands_component:add_command(new_command)
+	local water_tools = entity:get_component('stonehearth_ace:water_tools')
+	if water_tools then
+		local new_enabled = enabled
+		if new_enabled == nil then
+			new_enabled = not water_tools:get_enabled()
 		end
-
-		-- do anything else here like playing animations?
-
-		radiant.events.trigger(radiant, 'stonehearth_ace:on_water_gate_toggled', { water_gate = water_gate, enabled = enabled })
-		return true
+		water_tools:set_enabled(new_enabled)
 	end
-
-	return false
 end
 
 return WaterToolsCallHandler
