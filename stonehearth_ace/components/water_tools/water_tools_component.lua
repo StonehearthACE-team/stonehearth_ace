845d549e428f82ee050b8f3b87c3792922edc414
paulthegreat <paulthegreat@users.noreply.github.com>
2018-07-17 15:52:59 -0500

    improved water pump with child entity
    used firepit_component as inspiration for implementing invisible entity on top of the pump and better integrated the height property

--- stonehearth_ace/components/water_tools/water_tools_component.lua
+++ stonehearth_ace/components/water_tools/water_tools_component.lua
@@ -6,59 +6,127 @@ local log = radiant.log.create_logger('water_tools')
 
 local WaterToolsComponent = class()
 
--- Closely mimics wet_stone_component.lua
+-- Closely mimics wet_stone_component.lua and firepit_component.lua
 function WaterToolsComponent:initialize()
+	self._tick_listener = nil
+end
+
+function WaterToolsComponent:create()
 	local json = radiant.entities.get_json(self)
 	self._sv.enabled = false or json.enabled
 	self._sv.type = json.type
 	self._sv.on_command = json.on_command
 	self._sv.off_command = json.off_command
 	self._sv.rate = math.max(json and json.rate or 1, 0)
-	self._sv.height = math.max(json and json.height or 1, 1) + 1
+	self._sv.height = math.max(json and json.height or 1, 1)
 	self._sv.depth = math.max(json and json.depth or 0, 0)
+	self._sv._pump_child_entity = nil
+	self.__saved_variables:mark_changed()
+end
+
+function WaterToolsComponent:restore()
+	self._is_restore = true
+end
+
+function WaterToolsComponent:activate()
+	--Trace the parent to figure out if it's added or not:
+	self._parent_trace = self._entity:add_component('mob'):trace_parent('water tool added or removed')
+						:on_changed(function(parent_entity)
+								if not parent_entity then
+									--we were just removed from the world
+									self:_shutdown()
+								else
+									--we were just added to the world
+									self:_startup()
+								end
+							end)
 end
 
 function WaterToolsComponent:post_activate()
-	self:apply_settings(true, true)
-	
+	self:_on_enabled_changed()
+	self:_startup()
+
+	if self._is_restore then
+		self._parent_trace:push_object_state()
+	end
+end
+
+function WaterToolsComponent:destroy()
+   --When the water tool is destroyed, destroy any other child entities
+   self:_shutdown()
+end
+
+function WaterToolsComponent:_startup()
 	if self._sv.type == 'water_pump' then
+		local location = radiant.entities.get_world_grid_location(self._entity)
+		if not location then
+			log:error('could not get location of water pump')
+			return
+		end
+		location = Point3(location) + Point3(0, self:get_height(), 0)
+
+		if not self._sv._pump_child_entity then
+			-- if we don't already have a child entity for this pump, create it
+			self._sv._pump_child_entity = radiant.entities.create_entity('stonehearth_ace:gizmos:water_pump_topper', { owner = self._entity })
+			radiant.terrain.place_entity_at_exact_location(self._sv._pump_child_entity, location)
+		else
+			-- make sure the child entity is properly positioned
+			radiant.terrain.place_entity_at_exact_location(self._sv._pump_child_entity, location)
+		end
+		self.__saved_variables:mark_changed()
+	end
+
+	if not self._tick_listener then
 		self._tick_listener = radiant.events.listen(stonehearth.hydrology, 'stonehearth:hydrology:tick', function()
 			self:_on_tick()
 		end)
 	end
 end
 
-function WaterToolsComponent:apply_settings(enabled_changed)
-	if enabled_changed then
-		-- swap commands
-		local commands_component = self._entity:get_component('stonehearth:commands')
-		if commands_component then
-			if self._sv.enabled then
-				commands_component:remove_command(self._sv.on_command)
-				if not commands_component:has_command(self._sv.off_command) then
-					commands_component:add_command(self._sv.off_command)
-				end
-			else
-				commands_component:remove_command(self._sv.off_command)
-				if not commands_component:has_command(self._sv.on_command) then
-					commands_component:add_command(self._sv.on_command)
-				end
-			end
+function WaterToolsComponent:_shutdown()
+	if self._sv.type == 'water_pump' then
+		-- if we have a child entity for this pump, delete it
+		if self._sv._pump_child_entity then
+			radiant.entities.destroy_entity(self._sv._pump_child_entity)
+			self._sv._pump_child_entity = nil
 		end
-
-		if self._sv.type == 'water_gate' then
-			local new_collision_type
-			if self._sv.enabled then
-				new_collision_type = _radiant.om.RegionCollisionShape.PLATFORM
-			else
-				new_collision_type = _radiant.om.RegionCollisionShape.SOLID
-			end
-
-			self._entity:get_component('region_collision_shape'):set_region_collision_type(new_collision_type)
-		end
-
-		-- do anything else here like playing animations?
 	end
+
+	if self._tick_listener then
+		self._tick_listener:destroy()
+		self._tick_listener = nil
+	end
+end
+
+function WaterToolsComponent:_on_enabled_changed()
+	-- swap commands
+	local commands_component = self._entity:get_component('stonehearth:commands')
+	if commands_component then
+		if self._sv.enabled then
+			commands_component:remove_command(self._sv.on_command)
+			if not commands_component:has_command(self._sv.off_command) then
+				commands_component:add_command(self._sv.off_command)
+			end
+		else
+			commands_component:remove_command(self._sv.off_command)
+			if not commands_component:has_command(self._sv.on_command) then
+				commands_component:add_command(self._sv.on_command)
+			end
+		end
+	end
+
+	if self._sv.type == 'water_gate' then
+		local new_collision_type
+		if self._sv.enabled then
+			new_collision_type = _radiant.om.RegionCollisionShape.PLATFORM
+		else
+			new_collision_type = _radiant.om.RegionCollisionShape.SOLID
+		end
+
+		self._entity:get_component('region_collision_shape'):set_region_collision_type(new_collision_type)
+	end
+
+	-- do anything else here like playing animations?
 end
 
 function WaterToolsComponent:get_type()
@@ -74,7 +142,7 @@ function WaterToolsComponent:set_enabled(value)
 		self._sv.enabled = value
 		self.__saved_variables:mark_changed()
 
-		self:apply_settings(true)
+		self:_on_enabled_changed()
 
 		radiant.events.trigger(radiant, 'stonehearth_ace:on_water_tools_enabled_changed', { entity = self._entity, enabled = value })
 	end
@@ -93,8 +161,12 @@ function WaterToolsComponent:get_height()
 	return self._sv.height
 end
 
+function WaterToolsComponent:get_output_height()
+	return self._sv.height + 1
+end
+
 function WaterToolsComponent:set_height(value)
-	self._sv.height = math.max(value, 1) + 1
+	self._sv.height = math.max(value, 1)
 	self.__saved_variables:mark_changed()
 end
 
@@ -115,6 +187,12 @@ function WaterToolsComponent:destroy()
 end
 
 function WaterToolsComponent:_on_tick()
+	if self._sv.type == 'water_pump' then
+		self:_on_tick_water_pump()
+	end
+end
+
+function WaterToolsComponent:_on_tick_water_pump()
 	if not self._sv.enabled or self._sv.rate <= 0 then
 		return
 	end
@@ -124,33 +202,36 @@ function WaterToolsComponent:_on_tick()
 		return
 	end
 	
-	-- pull water up from the lowest depth first
+	-- identify the destination for the water
+	local output_location = location + Point3(0, self:get_output_height(), 0)
+
+	local rate = self._sv.rate
 	local total_volume_in = 0
+
+	-- pull water up from the lowest depth first
 	for depth = self._sv.depth, 0, -1 do
 		local source_location = location + Point3(0, -depth, 0)
 		local water_body = self:_get_water_body(source_location)
 
 		if water_body then
-			-- first we need to identify the destination for the water
-			local output_location = location + Point3(0, self._sv.height, 0)
-
-			-- then we need to remove water from where the entity is
-			local volume, info = stonehearth.hydrology:remove_water(self._sv.rate - total_volume_in, source_location, water_body)
-			total_volume_in = total_volume_in + volume
+			-- first we need to remove water from where the entity is
+			local volume_not_removed, info = stonehearth.hydrology:remove_water(rate - total_volume_in, source_location, water_body)
+			local volume_removed = rate - volume_not_removed
+			total_volume_in = total_volume_in + volume_removed
 
 			-- then we need to try adding the same amount of water that was removed to a position above the entity
-			local volume_left, info = stonehearth.hydrology:add_water(volume, output_location)
+			local volume_not_added, info = stonehearth.hydrology:add_water(volume_removed, output_location)
 
-			log:error('water pump removed %d water and added %d water', volume, volume - volume_left)
+			log:debug('water pump removed %d water and added %d water', volume_removed, volume_removed - volume_not_added)
 
 			-- finally, if we output less volume than we input, output the difference back at the source location
-			if volume_left > 0 then
-				stonehearth.hydrology:add_water(volume_left, source_location, water_body)
+			if volume_not_added > 0 then
+				stonehearth.hydrology:add_water(volume_not_added, source_location, water_body)
 				break
 			end
 		end
 
-		if total_volume_in >= self._sv.rate then
+		if total_volume_in >= rate then
 			break
 		end
 	end
