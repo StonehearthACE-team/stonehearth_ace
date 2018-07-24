0335eb8ee740f9d7d0fe3dfca2fc7971665300a3
DaniAngione <daniangi@gmail.com>
2018-07-24 01:10:14 -0300

    New Repo Organization (part 10)
    Finalized

--- stonehearth_ace/ai/task_groups/training_task_group.lua
+++
@@ -1,8 +0,0 @@
-local TrainingTaskGroup = class()
-TrainingTaskGroup.name = 'training'
-TrainingTaskGroup.does = 'stonehearth:work'
-TrainingTaskGroup.priority = 0.6
-
-return stonehearth.ai:create_task_group(TrainingTaskGroup)
-         :work_order_tag("job")
-         :declare_permanent_task('stonehearth_ace:training', {}, {0, 1})
