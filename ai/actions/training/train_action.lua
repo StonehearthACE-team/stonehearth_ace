de3efc19cd46d66daf06f1bcff191e3dd0684293
paulthegreat <paulthegreat@users.noreply.github.com>
2018-07-26 02:02:06 -0500

    added "manual" patrolling with priority over auto; also progress on training

--- ai/actions/training/train_action.lua
+++ ai/actions/training/train_action.lua
@@ -1,3 +1,4 @@
+local Point3 = _radiant.csg.Point3
 local Train = class()
 
 Train.name = 'train'
@@ -29,37 +30,66 @@ function Train:start_thinking(ai, entity, args)
 	ai:set_think_output()
 end
 
+function Train:stop_thinking(ai, entity, args)
+	
+end
+
 function find_training_dummy(entity)
 	return stonehearth.ai:filter_from_key('stonehearth_ace:training_dummy', entity:get_player_id(),
 		function(target)
 			if stonehearth.player:are_entities_friendly(entity, target) then
-				log:error('checking for training_dummy component')
 				return target:get_component('stonehearth_ace:training_dummy') ~= nil
 			end
 			return false
 		end)
 end
 
-function find_training_location(entity, target)
+function find_training_location(ai, entity, target)
 	local weapon = combat:get_main_weapon(entity)
-	local weapon_data = combat:get_main_weapon(entity)
+	local weapon_data = radiant.entities.get_entity_data(weapon, 'stonehearth:combat:weapon_data')
 	local min_range, max_range
 	if weapon_data.range then
 		-- it's a ranged combat class so get the range including any bonus range
-		min_range = 1
+		min_range = 5
 		max_range = combat:get_weapon_range(entity, weapon)
 	else
 		-- otherwise it's melee so get the reach
 		min_range, max_range = combat:get_melee_range(entity, weapon_data, target)
 	end
 
+	if not min_range or not max_range then
+		return radiant.entities.get_grid_in_front(target)
+		--ai:abort('entity has no weapon range')
+	end
+
 	-- determine the direction the target is facing
 	-- get a location at max_range in front of the target
 	-- test and go closer until it's a valid location (i.e., it has line of sight; don't worry about reachability for now)
 
-	local location = radiant.entities.get_world_grid_location(target)
+	local mob = target:get_component('mob')
+	local facing = radiant.math.round(mob:get_facing() / 90) * 90
+	local location = mob:get_world_grid_location()
+	local best_location = nil
 
-	return location
+	for distance = max_range, min_range, -0.5 do
+		local temp_location = get_location_in_front(location, facing, distance)
+		local line_of_sight = _physics:has_line_of_sight(target, Point3(temp_location.x, temp_location.y + 2, temp_location.z))
+		if line_of_sight then
+			best_location = temp_location
+			break
+		end
+	end
+
+	if best_location then
+		return best_location
+	else
+		ai:abort('cannot find suitable training location for this training dummy')
+	end
+end
+
+function get_location_in_front(location, facing, distance)
+	local offset = radiant.math.rotate_about_y_axis(-Point3(0, 0, distance), facing)
+	return location + offset
 end
 
 local ai = stonehearth.ai
@@ -71,8 +101,11 @@ return ai:create_compound_action(Train)
          :execute('stonehearth:drop_backpack_contents_on_ground', {})
 		 :execute('stonehearth:set_posture', { posture = 'stonehearth:combat' })
          :execute('stonehearth:find_best_reachable_entity_by_type', 
-					{ filter_fn = ai.CALL(find_training_dummy, ai.ENTITY), target = ai.ENTITY})
+					{ filter_fn = ai.CALL(find_training_dummy, ai.ENTITY)})
          :execute('stonehearth:reserve_entity', { entity = ai.PREV.item })
-         :execute('stonehearth:find_path_to_location', { location = ai.CALL(find_training_location, ai.ENTITY, ai.BACK(2).item), entity = ai.ENTITY })
+         :execute('stonehearth:find_path_to_location', { location = ai.CALL(find_training_location, ai, ai.ENTITY, ai.BACK(2).item) })
 		 :execute('stonehearth:follow_path', { path = ai.PREV.path })
-	-- now can we just tell our entity to attack the target, even though it's not an enemy?
+	-- now can we just tell our entity to attack the target, even though it's not an enemy? yes!
+		 :execute('stonehearth:combat:attack_melee_adjacent', { target = ai.BACK(4).item })	-- need to use ranged attack call if entity is ranged
+		 -- but we want them to keep attacking (and gaining experience) until it's "dead" or they find something better to do
+		 -- JobComponent:add_exp(value, add_curiosity_addition)
--- ai/task_groups/training_task_group.lua
+++ ai/task_groups/training_task_group.lua
@@ -5,4 +5,4 @@ TrainingTaskGroup.priority = 0.57
 
 return stonehearth.ai:create_task_group(TrainingTaskGroup)
          :work_order_tag("job")
-         :declare_permanent_task('stonehearth_ace:train', {}, {0, 1})
+         :declare_permanent_task('stonehearth_ace:train', {category = 'training'}, 1.0)
