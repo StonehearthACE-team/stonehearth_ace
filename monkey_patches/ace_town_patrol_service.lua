b1f895cc7a9d7e8cf6b5a3c5349d8328d03f0fa7
paulthegreat <paulthegreat@users.noreply.github.com>
2018-08-04 10:31:03 -0500

    auto patrol fix

--- monkey_patches/ace_town_patrol_service.lua
+++ monkey_patches/ace_town_patrol_service.lua
@@ -31,7 +31,7 @@ end
 -- Paul the Great: the trace callbacks aren't asynchronous, are they? do we have to worry about race conditions here?
 function AceTownPatrol:_switch_to_manual(player_id)
 	self._sv._auto_patrol[player_id] = false
-	self._sv._auto_patrollable_objects[player_id] = self:_get_patrollable_objects(player_id)
+	self:_get_auto_patrollable_objects(player_id) = self:_get_patrollable_objects(player_id)
 	self._sv._patrollable_objects[player_id] = {}
 
 	-- saved variables will get set by the calling function, don't need to set them here
