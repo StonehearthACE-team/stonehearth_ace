6c09d6e71c75b6f7c88742c23c630b4bd4c68114
paulthegreat <paulthegreat@users.noreply.github.com>
2019-03-16 04:22:38 -0500

    quick shepherd fix to animal finding

--- monkey_patches/ace_shepherd.lua
+++ monkey_patches/ace_shepherd.lua
@@ -8,8 +8,9 @@ AceShepherdClass._ace_old_can_find_animal_in_world = ShepherdClass.can_find_anim
 function AceShepherdClass:can_find_animal_in_world()
    if radiant.entities.has_buff(self._sv._entity, 'stonehearth_ace:buffs:shepherd:stenched_minor') then
       return false
-   end  
-   self:_ace_old_can_find_animal_in_world()  
+   end
+
+   return self:_ace_old_can_find_animal_in_world()
 end
 
 function AceShepherdClass:_on_resource_gathered(args)
@@ -17,11 +18,11 @@ function AceShepherdClass:_on_resource_gathered(args)
       local equipment_component = args.harvested_target:get_component('stonehearth:equipment')
       if equipment_component and equipment_component:has_item_type('stonehearth:pasture_equipment:tag') then
          self._job_component:add_exp(self._xp_rewards['harvest_animal'])
-		 if self:has_perk('improved_buffs') then
-         radiant.entities.add_buff(self._sv._entity, 'stonehearth_ace:buffs:shepherd:stenched_minor');
-		 else
-		 radiant.entities.add_buff(self._sv._entity, 'stonehearth:buffs:shepherd:stenched');
-		 end
+         if self:has_perk('improved_buffs') then
+            radiant.entities.add_buff(self._sv._entity, 'stonehearth_ace:buffs:shepherd:stenched_minor');
+         else
+            radiant.entities.add_buff(self._sv._entity, 'stonehearth:buffs:shepherd:stenched');
+         end
       end
    end
 end
@@ -33,11 +34,11 @@ function AceShepherdClass:_on_interacted_with_animal(animal)
       local buff_chance = compassion * stonehearth.constants.attribute_effects.COMPASSION_SHEPHERD_BUFF_CHANCE_MULTIPLIER
       local roll = rng:get_int(1, 100)  
       if roll <= buff_chance then
-	     if self:has_perk('improved_buffs') then
-         radiant.entities.add_buff(animal, 'stonehearth_ace:buffs:shepherd:compassionate_shepherd_major');
-		 else
-		 radiant.entities.add_buff(animal, 'stonehearth:buffs:shepherd:compassionate_shepherd');
-		 end        
+         if self:has_perk('improved_buffs') then
+            radiant.entities.add_buff(animal, 'stonehearth_ace:buffs:shepherd:compassionate_shepherd_major');
+         else
+            radiant.entities.add_buff(animal, 'stonehearth:buffs:shepherd:compassionate_shepherd');
+         end        
       end
    end
 end
