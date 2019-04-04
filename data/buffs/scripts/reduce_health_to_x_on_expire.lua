680977610297bbe59e82575775c8e231616c7086
paulthegreat <paulthegreat@users.noreply.github.com>
2019-04-04 15:53:46 -0500

    added reduce_health_to_x_on_expire buff script

---
+++ data/buffs/scripts/reduce_health_to_x_on_expire.lua
@@ -0,0 +1,15 @@
+local ReduceHealthOnExpire = class()
+
+function ReduceHealthOnExpire:on_buff_removed(entity, buff)
+   if buff and buff:is_duration_expired() then
+      local health = radiant.entities.get_health(entity)
+      local script_info = buff:get_json().script_info
+      local target_health = script_info and script_info.health or 0
+
+      if target_health < health then
+         radiant.entities.modify_health(entity, target_health - health)
+      end
+   end
+end
+
+return ReduceHealthOnExpire
