a4db98aff56ad603c3efb327e1d48ae8ae309194
paulthegreat <paulthegreat@users.noreply.github.com>
2019-09-05 23:10:02 -0500

    fixes to recipes_unlocked_can_start script
    had to override game_creation_service:create_camp_command to start the game master *after* initializing the town and applying reembarkation data

--- monkey_patches/ace_game_creation_service.lua
+++ monkey_patches/ace_game_creation_service.lua
@@ -1,8 +1,194 @@
+local Point2 = _radiant.csg.Point2
+local Point3 = _radiant.csg.Point3
 local validator = radiant.validator
 local constants = require 'stonehearth.constants'
 local NUM_STARTING_CITIZENS = constants.game_creation.num_starting_citizens
+local MIN_STARTING_ITEM_RADIUS = 0
+local MAX_STARTING_ITEM_RADIUS = 5
 
 local AceGameCreationService = class()
+
+-- ACE: moved starting the game master for this player to the end of the function instead of the middle
+function AceGameCreationService:create_camp_command(session, response, pt)
+   validator.expect_argument_types({'table'}, pt)
+   validator.expect.table.fields({'x', 'y', 'z'}, pt)
+
+   if validator.is_host_player(session) then
+      stonehearth.calendar:start()
+      stonehearth.hydrology:start()
+      stonehearth.mining:start()
+   end
+
+   stonehearth.world_generation:set_starting_location(Point2(pt.x, pt.z))
+
+   local facing = 180
+   local player_id = session.player_id
+   local town = stonehearth.town:get_town(player_id)
+   local pop = stonehearth.population:get_population(player_id)
+   local random_town_name = town:get_town_name()
+   local inventory = stonehearth.inventory:get_inventory(player_id)
+
+   -- place the stanfard in the middle of the camp
+   local location = Point3(pt.x, pt.y, pt.z)
+
+   local standard, standard_ghost = stonehearth.player:get_kingdom_banner_style(session.player_id)
+   if not standard then
+      standard = 'stonehearth:camp_standard'
+   end
+
+   local banner_entity = radiant.entities.create_entity(standard, { owner = player_id })
+   inventory:add_item(banner_entity)
+   radiant.terrain.place_entity(banner_entity, location, { facing = facing, force_iconic = false })
+   town:set_banner(banner_entity)
+
+   -- build the camp
+   local camp_x = pt.x
+   local camp_z = pt.z
+
+   local citizen_locations = {
+      {x=camp_x-3, z=camp_z-3},
+      {x=camp_x+0, z=camp_z-3},
+      {x=camp_x+3, z=camp_z-3},
+      {x=camp_x-3, z=camp_z+3},
+      {x=camp_x+3, z=camp_z+3},
+      {x=camp_x-3, z=camp_z+0},
+      {x=camp_x+3, z=camp_z+0},
+   }
+
+   if next(pop:get_generated_citizens()) == nil then
+      -- for quick start. TODO: make quick start integrate existing starting flow so we don't need to do this
+      self:_generate_initial_roster(pop)
+   end
+
+   -- get final citizens and destroy gender entity copies
+   local final_citizens = self:_get_final_citizens(pop, true)
+
+   local index = 1
+   local min_radius = 3
+   local max_radius = 7
+   local min_y = location.y - max_radius
+   for _, citizen in pairs(final_citizens) do
+      local citizen_location = citizen_locations[index]
+      if not citizen_location then
+         citizen_location = radiant.terrain.find_placement_point(location, min_radius, max_radius, citizen, 1, false)
+      end
+      self:_place_citizen_embark(citizen, citizen_location.x, citizen_location.z, { facing = facing, min_y = min_y })
+      index = index + 1
+   end
+
+   pop:unset_generated_citizens()
+
+   local town =  stonehearth.town:get_town(player_id)
+   town:check_for_combat_job_presence()
+
+   local camp_hearth_uri = pop:get_hearth() or 'stonehearth:decoration:firepit_hearth'
+   local hearth = self:_place_item(pop, camp_hearth_uri, camp_x, camp_z+5, { facing = facing, force_iconic = false, min_y = min_y })
+   inventory:add_item(hearth)
+   town:set_hearth(hearth)
+
+   local starting_resource = stonehearth.player:get_kingdom_starting_resource(player_id) or 'stonehearth:resources:wood:oak_log'
+   for i=1,2 do
+      local item = pop:create_entity(starting_resource)
+      self:try_place_entity_on_terrain(item, camp_x, camp_z, { min_y = min_y })
+      inventory:add_item(item)
+      if i <= NUM_STARTING_CITIZENS then
+         radiant.entities.pickup_item(final_citizens[i], item)
+      end
+   end
+
+   local game_options = pop:get_game_options()
+
+   if validator.is_host_player(session) then
+      -- Open game to remote players if specified
+      if game_options.remote_connections_enabled then
+         stonehearth.session_server:set_remote_connections_enabled(true)
+      end
+
+      -- Set max number of remote players if specified
+      if game_options.max_players then
+         stonehearth.session_server:set_max_players(game_options.max_players)
+      end
+
+      -- Set whether clients can control game speed
+      if game_options.game_speed_anarchy_enabled then
+         stonehearth.game_speed:set_anarchy_enabled(game_options.game_speed_anarchy_enabled)
+      end
+   end
+
+   -- Spawn initial items
+   local starting_items = radiant.entities.spawn_items(game_options.starting_items, location,
+      MIN_STARTING_ITEM_RADIUS, MAX_STARTING_ITEM_RADIUS, { owner = player_id })
+
+   -- add all the spawned items to the inventory, have citizens pick up items
+   local i = 3
+   for id, item in pairs(starting_items) do
+      inventory:add_item(item)
+      if i <= NUM_STARTING_CITIZENS then
+         radiant.entities.pickup_item(final_citizens[i], item)
+      end
+   end
+
+   -- kickstarter pets
+   if game_options.starting_pets then
+       for i, pet_uri in ipairs (game_options.starting_pets) do
+          local x_offset = -6 + i * 3;
+          self:_place_pet(pop, pet_uri, camp_x-x_offset, camp_z-6, { facing = facing })
+       end
+   end
+
+   -- Add starting gold
+   local starting_gold = game_options.starting_gold
+   if (starting_gold > 0) then
+      local inventory = stonehearth.inventory:get_inventory(player_id)
+      inventory:add_gold(starting_gold)
+   end
+
+   -- Handle re-embarkation
+   if game_options.reembark_spec then
+      -- Add items.
+      for _, item_spec in ipairs(game_options.reembark_spec.items) do
+         if item_spec.uri == 'stonehearth:loot:gold' then
+            inventory:add_gold(stonehearth.constants.reembarkation.gold_per_bag * item_spec.count)
+         else
+            local uri_exists = radiant.resources.load_json(item_spec.uri, true, false) ~= nil
+            if uri_exists then
+               local spawned_items = radiant.entities.spawn_items({ [item_spec.uri] =  item_spec.count }, location,
+                  MIN_STARTING_ITEM_RADIUS, MAX_STARTING_ITEM_RADIUS, { owner = player_id })
+               for _, item in pairs(spawned_items) do
+                  if item_spec.item_quality > 1 then
+                     item:add_component('stonehearth:item_quality'):initialize_quality(item_spec.item_quality)
+                  end
+                  inventory:add_item(item)
+               end
+            end
+         end
+      end
+
+      -- Unlock recipes.
+      for job_uri, recipe_keys in pairs(game_options.reembark_spec.recipes) do
+         local job_info = stonehearth.job:get_job_info(player_id, job_uri)
+         if job_info then  -- In case it was from a mod.
+            for _, recipe_key in ipairs(recipe_keys) do
+               if job_uri == 'stonehearth:jobs:farmer' then
+                  job_info:manually_unlock_crop(recipe_key, true)
+               else
+                  job_info:manually_unlock_recipe(recipe_key, true)
+               end
+            end
+         end
+      end
+   end
+
+   stonehearth.terrain:set_fow_enabled(player_id, true)
+
+   -- save that the camp has been placed
+   pop:place_camp()
+   
+   -- ACE: just moved this down here so everything else (especially reembarkation stuff) happens first
+   stonehearth.game_master:get_game_master(player_id):start()
+
+   return {random_town_name = random_town_name}
+end
 
 -- generate citizens and make a copy of each citizen for each gender
 function AceGameCreationService:generate_citizens_for_reembark_command(session, response, reembark_spec)
--- services/server/game_master/controllers/scripts/recipes_unlocked_can_start.lua
+++ services/server/game_master/controllers/scripts/recipes_unlocked_can_start.lua
@@ -1,30 +1,40 @@
 local RecipesUnlockedCanStart = class()
 
+local log = radiant.log.create_logger('recipes_unlocked_can_start')
+
 function RecipesUnlockedCanStart:start(ctx, info)
    local player_job_controller = stonehearth.job:get_player_job_controller(ctx.player_id)
    if not player_job_controller then
+      log:debug('no player job controller for player id "%s"', ctx.player_id)
       return false
    end
 
    for job, data in pairs(info.jobs) do
+      --log:debug('checking job %s for player %s', job, ctx.player_id)
       local job_info = player_job_controller:get_job(job)
       if job_info then
          local has_any_recipe = false
          local unlocked = job_info:get_manually_unlocked()
-         for _, recipe in pairs(data.recipes) do
+         --log:debug('has the following %s recipes unlocked: %s', radiant.size(unlocked), radiant.util.table_tostring(unlocked))
+         for _, recipe in ipairs(data.recipes) do
             local has_recipe = unlocked[recipe]
-            if data.type == 'any' and has_recipe then
+            if has_recipe then
                has_any_recipe = true
-               break
-            elseif data.type == 'all' and not has_recipe then
+               if data.type == 'any' then
+                  break
+               end
+            elseif data.type == 'all' then
+               log:debug('"%s" %s doesn\'t have recipe %s', ctx.player_id, job, recipe)
                return false
             end
          end
 
          if not has_any_recipe then
+            log:debug('"%s" %s doesn\'t have any of the checked recipes', ctx.player_id, job)
             return false
          end
       else
+         log:debug('"%s" doesn\'t have job info', ctx.player_id)
          return false
       end
    end
