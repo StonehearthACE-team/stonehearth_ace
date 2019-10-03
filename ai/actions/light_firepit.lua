43ad71ab738ad1de744337a5c93caa3f69e42004
paulthegreat <paulthegreat@users.noreply.github.com>
2019-10-03 15:04:10 -0500

    added preferred_ingredient material for crafting/firepits
    a preferred_ingredient ignores distance compared to equivalent quality non-preferred ingredients (if they're within the minimum distance, a non-preferred ingredient might still get used instead)

--- ai/actions/light_firepit.lua
+++ ai/actions/light_firepit.lua
@@ -15,6 +15,14 @@ function LightFirepit:start(ai, entity, args)
    ai:set_status_text_key('stonehearth:ai.actions.status_text.light_firepit', { target = args.firepit:get_entity() })
 end
 
+local _rating_fn = function(item)
+   if radiant.entities.is_material(item, 'preferred_ingredient') then
+      return 1
+   end
+
+   return 0
+end
+
 local ai = stonehearth.ai
 return ai:create_compound_action(LightFirepit)
             :execute('stonehearth:abort_on_event_triggered', {
@@ -23,6 +31,7 @@ return ai:create_compound_action(LightFirepit)
             })
             :execute('stonehearth:pickup_item_made_of', {
                material = ai.ARGS.firepit:get_fuel_material(),
+               rating_fn = _rating_fn,
                owner_player_id = ai.CALL(radiant.entities.get_work_player_id, ai.ENTITY)
             })
             :execute('stonehearth:drop_carrying_into_entity', { entity = ai.ARGS.firepit:get_entity() })
