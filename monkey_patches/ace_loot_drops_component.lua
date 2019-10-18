4ba45a8c5a6fb7ea6e3058b3c3f8ba4286c4ff94
paulthegreat <paulthegreat@users.noreply.github.com>
2019-10-18 00:57:40 -0500

    improved auto loot options on loot drops components

--- monkey_patches/ace_loot_drops_component.lua
+++ monkey_patches/ace_loot_drops_component.lua
@@ -15,9 +15,10 @@ function AceLootDropsComponent:_on_kill_event()
    if loot_table then
       local location = radiant.entities.get_world_grid_location(self._entity)
       if location then
-         local auto_loot
+         local auto_loot, force_auto_loot
          if self._sv.auto_loot_player_id then
             auto_loot = stonehearth.client_state:get_client_gameplay_setting(self._sv.auto_loot_player_id, 'stonehearth', 'auto_loot', false)
+            force_auto_loot = loot_table.force_auto_loot or self._entity:get_player_id() == self._sv.auto_loot_player_id
          end
          local town = stonehearth.town:get_town(self._sv.auto_loot_player_id)
 
@@ -27,9 +28,7 @@ function AceLootDropsComponent:_on_kill_event()
          end
          local items = LootTable(loot_table, quality)
                            :roll_loot()
-         local spawned_entities = radiant.entities.spawn_items(items, location, 1, 3, { owner = self._entity })
-
-         local quality = radiant.entities.get_item_quality(self._entity)
+         local spawned_entities = radiant.entities.spawn_items(items, location, 1, 3, { owner = force_auto_loot and self._sv.auto_loot_player_id or self._entity })
 
          --Add a loot command to each of the spawned items, or claim them automatically
          for id, entity in pairs(spawned_entities) do
@@ -38,9 +37,7 @@ function AceLootDropsComponent:_on_kill_event()
             if entity_forms then
                target = entity_forms:get_iconic_entity()
             end
-            if self._sv.auto_loot_player_id and (loot_table.force_auto_loot or self._entity:get_player_id() == self._sv.auto_loot_player_id) then
-               entity:set_player_id(self._sv.auto_loot_player_id)
-               target:set_player_id(self._sv.auto_loot_player_id)
+            if force_auto_loot then
                stonehearth.inventory:get_inventory(self._sv.auto_loot_player_id)
                                        :add_item_if_not_full(entity)
             else
