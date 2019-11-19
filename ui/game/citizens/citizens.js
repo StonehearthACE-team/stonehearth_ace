f1013b5898ac2820483df3850180d9b9980bfadb
paulthegreat <paulthegreat@users.noreply.github.com>
2019-11-18 19:25:03 -0600

    when citizen view shown, updates all citizen rows

--- ui/game/citizens/citizens.js
+++ ui/game/citizens/citizens.js
@@ -1,4 +1,25 @@
 App.StonehearthCitizensView.reopen({
+   didInsertElement: function() {
+      var self = this;
+      self._firstTime = true;
+      self._super();
+   },
+
+   _onVisibilityChanged: function() {
+      var self = this;
+      if (self._firstTime) {
+         self._firstTime = null;
+         return;
+      }
+
+      if (self.get('isVisible')) {
+         // tell each row to update
+         self._containerView.forEach(function(item, index, enumerable) {
+            item.updateRow();
+         });
+      }
+   }.observes('isVisible'),
+
    actions: {
       showPromotionTree: function(citizen) {
          App.stonehearthClient.showPromotionTree(citizen.__self, citizen['stonehearth:job'].job_index);
@@ -30,5 +51,15 @@ App.StonehearthCitizenTasksRowView.reopen({
       var self = this;
       $(top).off('radiant_selection_changed', self.selection_event_func);
       self._super();
+   },
+
+   updateRow: function() {
+      var self = this;
+
+      self._update();
+      self._onWorkingForChanged();
+      self._updateMoodTooltip();
+      self._updateDescriptionTooltip();
+      self._onJobChanged();
    }
 });
