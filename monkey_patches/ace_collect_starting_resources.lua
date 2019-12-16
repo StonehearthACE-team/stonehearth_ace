27cf060f19a3d6162a450128cdf94cd43e247565
paulthegreat <paulthegreat@users.noreply.github.com>
2019-12-16 00:06:22 -0600

    fix to typo resulting in nil error

--- monkey_patches/ace_collect_starting_resources.lua
+++ monkey_patches/ace_collect_starting_resources.lua
@@ -4,7 +4,7 @@ local BaseScenario = require 'stonehearth.scenarios.basic_tutorial_scenario'
 function AceCollectResourcesTutorialScenario:destroy()
    self:_remove_listeners()
    self:_destroy_bulletin()
-   BaseScenario.__userdestroy(self)
+   BaseScenario.__user_destroy(self)
 end
 
 return AceCollectResourcesTutorialScenario
