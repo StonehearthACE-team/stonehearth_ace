d61e3894487cebc1dea1b0504345587716df2e06
paulthegreat <paulthegreat@users.noreply.github.com>
2020-10-15 02:18:32 -0500

    fix to some rest_from_injuries bed filters

--- ai/actions/health/goto_other_owned_bed_action.lua
+++ ai/actions/health/goto_other_owned_bed_action.lua
@@ -6,26 +6,28 @@ GotoOtherOwnedBed.args = {}
 GotoOtherOwnedBed.priority = 0
 
 function GotoOtherOwnedBed:start_thinking(ai, entity, args)
-   self._entity_id = entity:get_id()
-end
-
-local function make_is_available_bed_filter()
-   return stonehearth.ai:filter_from_key('stonehearth:rest_from_injuries:rest_in_bed', 'none', function(target)
-         local bed_data = radiant.entities.get_entity_data(target, 'stonehearth:bed')
-         if bed_data and not target:add_component('stonehearth:mount'):is_in_use() then
-            local ownable_component = target:get_component('stonehearth:ownable_object')
-            local owner = ownable_component:get_owner()
-            if owner and owner:get_id() ~= self._entity_id then
-               return true
-            end
+   local entity_id = entity:get_id()
+   
+   local filter_fn = stonehearth.ai:filter_from_key('stonehearth:rest_from_injuries:rest_in_bed', 'none', function(target)
+      local bed_data = radiant.entities.get_entity_data(target, 'stonehearth:bed')
+      if bed_data and not target:add_component('stonehearth:mount'):is_in_use() then
+         local ownable_component = target:get_component('stonehearth:ownable_object')
+         local owner = ownable_component:get_owner()
+         if owner and owner:get_id() ~= entity_id then
+            return true
          end
-         return false
-      end)
+      end
+      return false
+   end)
+
+   ai:set_think_output({
+      filter_fn = filter_fn
+   })
 end
 
 local ai = stonehearth.ai
 return ai:create_compound_action(GotoOtherOwnedBed)
          :execute('stonehearth:goto_entity_type', {
-            filter_fn = make_is_available_bed_filter(),
+            filter_fn = ai.PREV.filter_fn,
             description = 'rest in other-owned bed'
          })
