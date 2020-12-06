29a509b445cf71fc069ca98023833d96afe6db22
DaniAngione <daniangi@gmail.com>
2020-12-06 01:12:10 -0300

    some planter changes, trader changes, splitting block fix

--- ai/task_groups/planters_task_group.lua
+++ ai/task_groups/planters_task_group.lua
@@ -5,7 +5,7 @@ PlantersTaskGroup.priority = { 0, 0.84 }
 
 return stonehearth.ai:create_task_group(PlantersTaskGroup)
          :work_order_tag("job")
-         :declare_permanent_task('stonehearth_ace:harvest_herbalist_planter', {}, 1.0)
-         :declare_multiple_tasks('stonehearth_ace:plant_herbalist_planter', 0.75)
+			:declare_multiple_tasks('stonehearth_ace:plant_herbalist_planter', 1.0)
+         :declare_permanent_task('stonehearth_ace:harvest_herbalist_planter', {}, 0.8)        
          :declare_multiple_tasks('stonehearth_ace:clear_herbalist_planter', 0.75)
          :declare_permanent_task('stonehearth_ace:tend_herbalist_planter', {}, 0.5)
