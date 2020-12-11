78432d81acb6dba33e8acc0f9a325a7e8d392f87
paulthegreat <paulthegreat@users.noreply.github.com>
2020-12-11 16:06:22 -0600

    fix to game hotkeys disabling ui toggle

--- ui/game/game.js
+++ ui/game/game.js
@@ -1,28 +1,31 @@
+var gameHotkeys = [];
+$.getJSON('/stonehearth_ace/ui/data/game_hotkeys.json', function(data) {
+   var hotkeys = [];
+   radiant.each(data, function(k, v) {
+      if (v) {
+         v.key = k;
+         hotkeys.push(v);
+      }
+   });
+
+   gameHotkeys = hotkeys;
+});
+
 App.StonehearthGameUiView.reopen({
    didInsertElement: function () {
       var self = this;
 
-      $.getJSON('/stonehearth_ace/ui/data/game_hotkeys.json', function(data) {
-         var hotkeys = [];
-         radiant.each(data, function(k, v) {
-            if (v) {
-               v.key = k;
-               hotkeys.push(v);
+      gameHotkeys.forEach(keyData => {
+         var key = keyData.key;
+         var btn = $(`<button style="display: none" hotkey_action="${key}">${key}</button>`);
+         self.$().append(btn);
+         btn.click(function(e) {
+            if (keyData.event) {
+               $(top).trigger(keyData.event, keyData.eventArgs)
             }
          });
-
-         hotkeys.forEach(keyData => {
-            var key = keyData.key;
-            var btn = $(`<button style="display: none" hotkey_action="${key}">${key}</button>`);
-            self.$().append(btn);
-            btn.click(function(e) {
-               if (keyData.event) {
-                  $(top).trigger(keyData.event, keyData.eventArgs)
-               }
-            });
-         });
-         
-         self._super();
       });
+
+      self._super();
    },
 });
