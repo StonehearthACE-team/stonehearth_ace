0fb98591d5b5c22e35aa481b29292db107d1710e
paulthegreat <paulthegreat@users.noreply.github.com>
2020-12-12 12:35:30 -0600

    added command json option visible_in_citizens_view, defaults to true

--- ui/game/citizens/citizens.js
+++ ui/game/citizens/citizens.js
@@ -20,6 +20,22 @@ App.StonehearthCitizensView.reopen({
       }
    }.observes('isVisible'),
 
+   selected_commands: function() {
+      var filter_fn = function(uri, data) {
+         if (data.visible_in_citizens_view === false) {
+            return false;
+         }
+      };
+      var commands = radiant.map_to_array(this.get('selected.stonehearth:commands.commands'), filter_fn);
+      commands.sort(function(a, b){
+         var aName = a.ordinal ? a.ordinal : 0;
+         var bName = b.ordinal ? b.ordinal : 0;
+         var n = bName - aName;
+         return n;
+      });
+      return commands;
+   }.property('selected.stonehearth:commands.commands'),
+
    actions: {
       showPromotionTree: function(citizen) {
          App.stonehearthClient.showPromotionTree(citizen.__self, citizen['stonehearth:job'].job_index);
