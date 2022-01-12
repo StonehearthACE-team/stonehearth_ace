8b02856aadecb9c69e4deeea59844f067d376693
DaniAngione <daniangi@gmail.com>
2022-01-12 01:00:52 -0300

    Continued the dragonborn quest (almost done, just invasions left)
    some fixes to recipes using crops not available anymore; fixes to mixintos not working properly; fix to inventory check script not considering iconic placeables; slightly reduced the sound of waterfalls and bee skeps

--- services/server/game_master/controllers/scripts/inventory_check_script.lua
+++ services/server/game_master/controllers/scripts/inventory_check_script.lua
@@ -19,6 +19,8 @@ function InventoryCheck:start(ctx, info)
 
    local items = inventory:get_item_tracker('stonehearth:basic_inventory_tracker')
                               :get_tracking_data()
+   local placeable_items = inventory:get_item_tracker('stonehearth:placeable_item_inventory_tracker')
+                                          :get_tracking_data()
    local total_item_count = 0
    local all_matching_items = {}                           
    local check = info.check
@@ -31,9 +33,13 @@ function InventoryCheck:start(ctx, info)
             local matching_items = items:get(uri)
             total_item_count = total_item_count + matching_items.count
          end
+         if placeable_items:contains(uri) then
+            local matching_placeable_items = placeable_items:get(uri)
+            total_item_count = total_item_count + matching_placeable_items.count
+         end
       end
    elseif materials then
-      local keys = items:get_keys()
+      local keys, placeable_keys = items:get_keys() and placeable_items:get_keys()
       for _, uri in pairs(keys) do
          local matching_items = items:get(uri)
          local _, entity = next(matching_items.items)
@@ -49,6 +55,24 @@ function InventoryCheck:start(ctx, info)
             if match then
                all_matching_items[uri] = matching_items
                total_item_count = total_item_count + matching_items.count
+            end
+         end
+      end
+      for _, uri in pairs(placeable_keys) do
+         local matching_placeable_items = placeable_items:get(uri)
+         local _, entity = next(matching_placeable_items.items)
+         if entity then
+            local match = true
+            for _, material in pairs(materials) do
+               if not radiant.entities.is_material(entity, material) then
+                  match = false
+                  break
+               end
+            end           
+
+            if match then
+               all_matching_items[uri] = matching_placeable_items
+               total_item_count = total_item_count + matching_placeable_items.count
             end
          end
       end
