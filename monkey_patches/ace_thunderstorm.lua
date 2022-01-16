f3f471462e3b50e4370337df737a8de41d3a51f3
DaniAngione <daniangi@gmail.com>
2022-01-16 19:48:17 -0300

    Fix to wheat, some extra skins for cult, fix to fuel filtering, thunderstorm broken script

---
+++ monkey_patches/ace_thunderstorm.lua
@@ -0,0 +1,80 @@
+local Point3 = _radiant.csg.Point3
+local Cube3 = _radiant.csg.Cube3
+local WeightedSet = require 'stonehearth.lib.algorithms.weighted_set'
+local rng = _radiant.math.get_default_rng()
+
+local AceThunderstormWeather = class()
+
+local LIGHTNING_INTERVAL = '15m+30m'
+local LIGHTNING_EFFECTS = {
+   'stonehearth:effects:lightning_effect',
+   'stonehearth:effects:lightning_effect2'
+}
+local LIGHTNING_GROUND_EFFECT = 'stonehearth:effects:lightning_impact_ground'
+local LIGHTNING_TREE_EFFECT = 'stonehearth:effects:lightning_impact_tree'
+local LIGHTNING_DAMAGE = 30
+local LIGHTNING_TARGETS_SET = WeightedSet(rng)
+LIGHTNING_TARGETS_SET:add('SPOT', 25)
+LIGHTNING_TARGETS_SET:add('CITIZEN', 1)
+local TREE_SEARCH_RADIUS = 3
+
+function AceThunderstormWeather:_spawn_lightning()
+   local effect = LIGHTNING_EFFECTS[rng:get_int(1, #LIGHTNING_EFFECTS)]
+   local target = LIGHTNING_TARGETS_SET:choose_random()
+   if target == 'CITIZEN' and stonehearth.game_creation:get_game_mode() ~= 'stonehearth:game_mode:peaceful' then
+      local citizen = self:_select_random_player_character(effect)
+      if citizen then
+         local location = radiant.entities.get_world_grid_location(citizen)
+         if location and not stonehearth.terrain:is_sheltered(location) then
+            radiant.effects.run_effect(citizen, effect)
+            radiant.entities.modify_health(citizen, -LIGHTNING_DAMAGE)
+            radiant.entities.add_buff(citizen, 'stonehearth:buffs:weather:hit_by_lightning')
+         end
+      end
+   elseif target == 'SPOT' then
+      -- Choose a point to hit.
+      local terrain_bounds = stonehearth.terrain:get_bounds()
+      local x = rng:get_int(terrain_bounds.min.x, terrain_bounds.max.x)
+      local z = rng:get_int(terrain_bounds.min.z, terrain_bounds.max.z)
+
+      -- Find a tree to hit near our chosen point.
+      local tree
+      local search_cube = Cube3(Point3(x - TREE_SEARCH_RADIUS, terrain_bounds.min.y, z - TREE_SEARCH_RADIUS),
+                                Point3(x + TREE_SEARCH_RADIUS, terrain_bounds.max.y, z + TREE_SEARCH_RADIUS))
+      for _, item in pairs(radiant.terrain.get_entities_in_cube(search_cube)) do
+         local catalog_data = stonehearth.catalog:get_catalog_data(item:get_uri()) or {}
+         if item:get_component('stonehearth:resource_node') and catalog_data.category == 'plants' then
+            tree = item
+            break
+         end
+      end
+
+      if tree then
+         local location = radiant.entities.get_world_grid_location(tree)
+         self:_spawn_effect_at(location, effect)
+         self:_spawn_effect_at(location, LIGHTNING_TREE_EFFECT)
+         -- ACE Changes start here; we don't want trees to drop their resources anymore. We'll drop some charcoal now, just for fun!
+         radiant.entities:destroy_entity(tree)
+         -- ACE Changes end here
+      else
+         local center = Point3(x, terrain_bounds.max.y, z)
+         local target = Point3(x, terrain_bounds.min.y, z)
+         local ground_point = _physics:shoot_ray(center, target, true, 0)
+
+         -- Don't hit water.
+         local search_cube = Cube3(ground_point - Point3(1, 2, 1),
+                                   ground_point + Point3(1, 2, 1))
+         local is_in_water = next(radiant.terrain.get_entities_in_cube(search_cube, function(e)
+               return e:get_component('stonehearth:water') ~= nil
+            end)) ~= nil
+         if not is_in_water then
+            ground_point.y = ground_point.y + 1  -- On top of the terain voxel.
+            self:_spawn_effect_at(ground_point, effect)
+            self:_spawn_effect_at(ground_point, LIGHTNING_GROUND_EFFECT)
+         end
+      end
+   end
+   self._sv._lightning_timer = stonehearth.calendar:set_persistent_timer('thunderstorm ligtning', LIGHTNING_INTERVAL, radiant.bind(self, '_spawn_lightning'))
+end
+
+return AceThunderstormWeather
--- stonehearth_ace_server.lua
+++ stonehearth_ace_server.lua
@@ -179,6 +179,7 @@ local monkey_patches = {
    ace_tentacle_snared_debuff = 'stonehearth.data.buffs.tentacle_snared.tentacle_snared_debuff',
    ace_terrain_patch_component = 'stonehearth.components.terrain_patch.terrain_patch_component',
    ace_terrain_service = 'stonehearth.services.server.terrain.terrain_service',
+   ace_thunderstorm = 'stonehearth.data.weather.thunderstorm.thunderstorm',
    ace_town = 'stonehearth.services.server.town.town',
    ace_town_patrol_service = 'stonehearth.services.server.town_patrol.town_patrol_service',
    ace_town_service = 'stonehearth.services.server.town.town_service',
