622c587610dbf80dd624bf2cafe7615abb2a68f5
DaniAngione <daniangi@gmail.com>
2022-02-06 17:20:10 -0300

    some health changes (names to not be confusing with wounds, health thresholds...)
    Attempted "Wound thoughts" on health observer (not working yet); some model changes and dragon stuff

--- monkey_patches/ace_health_observer.lua
+++ monkey_patches/ace_health_observer.lua
@@ -1,4 +1,6 @@
 local HealthObserver = require 'stonehearth.ai.observers.health_observer'
+local constants = require 'stonehearth.constants'
+
 local AceHealthObserver = class()
 
 local log = radiant.log.create_logger('health_observer')
@@ -21,6 +23,63 @@ function AceHealthObserver:destroy()
    if self._magically_healed_listener then
       self._magically_healed_listener:destroy()
       self._magically_healed_listener = nil
+   end
+end
+
+AceHealthObserver._ace_old__update = HealthObserver._update
+function AceHealthObserver:_update(initial_update)
+   self:_ace_old__update(initial_update)
+   self:_check_update_wound_thoughts()
+end
+
+function AceHealthObserver:_check_update_thoughts(percentage, last_percentage)
+   if percentage >= .99 then
+      radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health:neutral')
+   elseif percentage >= .9 then
+      if not (last_percentage >= .9 and last_percentage < .99) then
+         radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health:pain_minor')
+      end
+   elseif percentage >= .75 then
+      if not (last_percentage >= .75 and last_percentage < .9) then
+         radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health:pain_scratched')
+      end
+   elseif percentage >= .5 then
+      if not (last_percentage >= .5 and last_percentage < .75) then
+         radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health:pain_mild')
+      end
+   elseif percentage >= .25 then
+      if not (last_percentage >= .25 and last_percentage < .5) then
+         radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health:pain_moderate')
+      end
+   elseif not (last_percentage < .25) then
+      radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health:pain_severe')
+   end
+end
+
+function AceHealthObserver:_check_update_wound_thoughts()
+   local highest_rank = 0
+   for type in pairs(constants.health.WOUND_TYPES) do
+      if self._sv.entity:get_component('stonehearth:buffs'):has_category_buffs(type) then
+         local type_buffs = self._sv.entity:get_component('stonehearth:buffs'):get_buffs_by_category(type)
+         for buff_id, buff in pairs(type_buffs) do
+            local new_rank = buff:get_rank()
+            if new_rank > highest_rank then
+               highest_rank = new_rank
+            end
+         end
+      end
+   end
+   
+   if highest_rank > 1 then
+      if highest_rank >= 4 then
+         radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health_wounds:wound_extreme')
+      elseif highest_rank == 3 then
+         radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health_wounds:wound_high')
+      elseif highest_rank == 2 then
+         radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health_wounds:wound_medium')
+      end
+   else
+      radiant.entities.add_thought(self._sv.entity, 'stonehearth:thoughts:health_wounds:no_wound')
    end
 end
 
