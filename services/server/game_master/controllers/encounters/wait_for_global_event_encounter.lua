7ad8b364f3ea7e1df48f21980a109d59f52ef534
paulthegreat <paulthegreat@users.noreply.github.com>
2022-11-20 15:10:33 -0600

    fixed/streamlined wait_for_global_event_encounter

--- services/server/game_master/controllers/encounters/wait_for_global_event_encounter.lua
+++ services/server/game_master/controllers/encounters/wait_for_global_event_encounter.lua
@@ -4,77 +4,37 @@ function WaitForGlobalEventEncounter:initialize()
    self._sv.source = nil
    self._sv.ctx = nil
    self._sv.event = nil
-   self._sv.is_multi = false
    self._sv.is_repeatable = false
-   self._sv.is_trigger_on_each = false
    self._log = radiant.log.create_logger('game_master.encounters.wait_for_global_event')
 end
 
 function WaitForGlobalEventEncounter:activate()
    if self._sv.source then
-      if self._sv.is_multi then
-         self:_listen_on_multiple_sources()
-      else
-         self:_listen_for_event()
-      end
+      self:_listen_for_event()
    end
 end
 
 function WaitForGlobalEventEncounter:start(ctx, info)
    local event = info.event
-   local source = info.source
+   local source = info.source and self:_get_source(info.source)
 
-   assert(event)
+   assert(event and source)
    
-   -- Grab the appropriate entities for each specified source string
-   if type(source) == 'table' then
-      local sources = {}
-      for _, single_source in pairs(source) do
-         single_source = self:_get_source(single_source)
-         if type(single_source) == 'table' then
-            for _, single_source_entry in pairs(single_source) do
-               table.insert(sources, single_source_entry)
-            end
-         else
-            table.insert(sources, single_source)
-         end
-      end
-      source = sources
-   else
-      source = source and self:_get_source(source)
-   end
-
-   -- Check if source (ctx or directly from json) is a table of entities
-   self._sv.is_multi = type(source) == 'table'
    self._sv.is_repeatable = info.repeatable or false
-   self._sv.is_trigger_on_each = info.trigger_on_each or false
    
-   -- if multiple sources, add event listener to each source
-   -- and trigger next encounter if all listeners are triggered
-   if self._sv.is_multi then
-      if self:_check_sources_are_valid(source) then
-         self._sv.ctx = ctx
-         self._sv.source = source
-         self._sv.event = event
-         self.__saved_variables:mark_changed()
+   self._sv.ctx = ctx
+   self._sv.source = source
+   self._sv.event = event
+   self.__saved_variables:mark_changed()
 
-         self:_listen_on_multiple_sources()
-      end
-   elseif not source or source:is_valid() then
-      self._sv.ctx = ctx
-      self._sv.source = source or stonehearth.game_master:get_game_master(ctx.player_id)
-      self._sv.event = event
-      self.__saved_variables:mark_changed()
-
-      self:_listen_for_event()
-   end
+   self:_listen_for_event()
 end
 
 function WaitForGlobalEventEncounter:_get_source(source)
    -- try to resolve the source globally; e.g., 'stonehearth' or 'stonehearth_ace.mercantile'
    -- defaults to 'radiant'
    local default = radiant
-   local fn = string.format('return function() return %s end', path)
+   local fn = string.format('return function() return %s end', source)
    local f, error = loadstring(fn)
    if f == nil then
       -- parse error?  no problem!
@@ -104,37 +64,6 @@ function WaitForGlobalEventEncounter:_listen_for_event()
          self._log:debug('"%s" event on "%s" triggered!', tostring(source), event)
          self:_on_event_triggered(ctx)
       end)
-end
-
-function WaitForGlobalEventEncounter:_listen_on_multiple_sources()
-   local event = self._sv.event
-   local sources = self._sv.source
-   local num_sources = radiant.size(sources)
-   local listeners_triggered = 0
-   self._listeners = {}
-
-   for _,source in pairs(sources) do
-      self._log:debug('listening for "%s" event on "%s"', event, tostring(source))
-      table.insert(self._listeners, radiant.events.listen(source, event, function()
-         local ctx = self._sv.ctx
-         self._log:debug('"%s" event on "%s" triggered!', tostring(source), event)
-         listeners_triggered = listeners_triggered + 1
-         if listeners_triggered == num_sources or self._sv.is_trigger_on_each then
-            self:_on_event_triggered(ctx)
-         end
-      end))
-   end
-
-end
-
--- check whether all sources are valid
-function WaitForGlobalEventEncounter:_check_sources_are_valid(sources)
-   for _,source in pairs(sources) do
-      if not source:is_valid() then
-         return false
-      end
-   end
-   return true
 end
 
 function WaitForGlobalEventEncounter:_on_event_triggered(ctx)
@@ -180,7 +109,8 @@ return WaitForGlobalEventEncounter
    "out_edge" : "<out edge>",
 
    "wait_for_global_event_info" :  {
-      "source" : "create_thief_camp.entities.campfire.campfire",
+      "mod_root": "stonehearth_ace",
+      "source" : "radiant",
       "event"  : "radiant:entity:pre_destroy"
    }
 }
@@ -200,16 +130,9 @@ return WaitForGlobalEventEncounter
                "type": "object",
                "mod_root": "stonehearth_ace",
                "properties": {
-                  "source": {
-                     "anyOf": [
-                        {"$ref": "elements/context_path.json"},
-                        { "type": "array", "items": {"$ref": "elements/context_path.json"}}
-                     ],
-                     "description": "If unspecified, listens on the owner player's game master."
-                  },
+                  "source": { "type": "string" },
                   "event": { "type": "string" },
-                  "repeatable": { "type": "boolean", "description": "If true, continues to listen and trigger out edges until destroyed." },
-                  "trigger_on_each": { "type": "boolean", "description": "If true, and the source is a table, triggers out edges when any entry has the event triggered, instead of when all sources have had the event triggered. Usually used with repeatable == true" }
+                  "repeatable": { "type": "boolean", "description": "If true, continues to listen and trigger out edges until destroyed." }
                },
                "required": ["event"],
                "additionalProperties": false
