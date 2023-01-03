e351a1cc63b1eef97ffe1c1b46e52ad6479ab299
paulthegreat <paulthegreat@users.noreply.github.com>
2023-01-03 01:33:06 -0600

    probably pointless ai tweak

--- ai/actions/siege/basic_wait_at_siege_object.lua
+++ ai/actions/siege/basic_wait_at_siege_object.lua
@@ -11,6 +11,13 @@ WaitAtSiegeObject.args = {
 WaitAtSiegeObject.priority = 0
 
 function WaitAtSiegeObject:start_thinking(ai, entity, args)
+   if not stonehearth.combat:is_killable_target_of_type(args.target, 'siege') then
+      return
+   end
+   if stonehearth.combat:get_assaulting(entity) or stonehearth.combat:get_defending(entity) then
+      return -- dont wait at siege object if we are attacking something or being attacked
+   end
+
    ai:set_think_output({ target = args.target })
 end
 
