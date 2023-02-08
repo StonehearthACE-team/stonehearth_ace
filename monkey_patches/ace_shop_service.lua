f0d98d1c3f5b88528a90142bf005f9720ffccd66
DaniAngione <daniangi@gmail.com>
2023-02-08 18:59:24 -0300

    Update ace_shop_service.lua

--- monkey_patches/ace_shop_service.lua
+++ monkey_patches/ace_shop_service.lua
@@ -9,4 +9,46 @@ function AceShopService:trigger_trader_encounter_command(session, response, shop
    end
 end
 
+function AceShopService:_item_in_material_filter(entity_description, filter)
+   if not filter.material and not filter.exclude_material then
+      return true
+   end
+
+   --Record all the materials for this entity
+   local materials = {}
+   local tags = entity_description.materials or {}
+   if radiant.util.is_string(tags) then
+      tags = radiant.util.split_string(tags)
+   end
+   for _, tag in ipairs(tags) do
+      materials[tag] = true
+   end
+
+   -- ACE addition: exclusion filters
+   if filter.exclude_material then
+      for _, material in pairs(filter.exclude_material) do
+         if material and materials[material] then
+            return false
+         end
+      end
+   end
+
+   --for each material in the filter, check if the entity has it
+   --the entity must match all the items in the filter, or it will fail the filter
+   if filter.material then
+      for _, material in pairs(filter.material) do
+         if material then
+            local tags = radiant.util.split_string(material)
+            for _, tag in ipairs(tags) do
+               if not materials[tag] then
+                  return false
+               end
+            end
+         end
+      end
+   end
+
+   return true
+end
+
 return AceShopService
