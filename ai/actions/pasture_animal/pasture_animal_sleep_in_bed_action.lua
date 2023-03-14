488c38420db0b60e12ee2d343d0f5c483cd89728
paulthegreat <paulthegreat@users.noreply.github.com>
2023-03-13 20:01:44 -0500

    simplified pasture animal sleeping in beds

--- ai/actions/pasture_animal/pasture_animal_sleep_in_bed_action.lua
+++ ai/actions/pasture_animal/pasture_animal_sleep_in_bed_action.lua
@@ -1,4 +1,4 @@
-local animal_filters = require 'ai.filters.animal_filters'
+local animal_filters = require 'stonehearth_ace.ai.filters.animal_filters'
 local PastureAnimalSleepInBed = radiant.class()
 
 PastureAnimalSleepInBed.name = 'sleep in pasture bed'
@@ -6,24 +6,11 @@ PastureAnimalSleepInBed.does = 'stonehearth_ace:pasture_animal_sleep_in_bed'
 PastureAnimalSleepInBed.args = {}
 PastureAnimalSleepInBed.priority = 0
 
-local function _pet_bed_rating_filter(entity)
-   local pasture_animal_location = radiant.entities.get_world_grid_location(entity)
-   return function(bed)
-      local bed_location = radiant.entities.get_world_grid_location(bed)
-      return -pasture_animal_location:distance_to_squared(bed_location)
-   end
-end
-
 local ai = stonehearth.ai
 return ai:create_compound_action(PastureAnimalSleepInBed)
-         :execute('stonehearth:find_best_reachable_entity_by_type', {
+         :execute('stonehearth:goto_entity_type', {
             filter_fn = ai.CALL(animal_filters.make_available_pet_bed_filter, ai.ENTITY),
-            rating_fn = ai.CALL(_pet_bed_rating_filter, ai.ENTITY),
             description = 'find best pet bed',
          })
-         :execute('stonehearth:find_path_to_reachable_entity', {
-            destination = ai.PREV.item
-         })
-         :execute('stonehearth:follow_path', { path = ai.PREV.path })
-         :execute('stonehearth:reserve_entity', { entity = ai.BACK(3).item })
-         :execute('stonehearth_ace:pasture_animal_sleep_in_bed_adjacent', { bed = ai.BACK(4).item })
+         :execute('stonehearth:reserve_entity', { entity = ai.BACK(1).destination_entity })
+         :execute('stonehearth_ace:pasture_animal_sleep_in_bed_adjacent', { bed = ai.BACK(1).entity })
