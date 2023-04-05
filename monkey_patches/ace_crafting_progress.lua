dd47a934bba9eaa4d5d24f56e74c4ece0d07edb5
paulthegreat <paulthegreat@users.noreply.github.com>
2023-04-05 02:49:19 -0500

    working ingredient fixes

--- monkey_patches/ace_crafting_progress.lua
+++ monkey_patches/ace_crafting_progress.lua
@@ -1,10 +1,10 @@
 local CraftingProgress = radiant.mods.require('stonehearth.components.workshop.crafting_progress')
 local AceCraftingProgress = radiant.class()
 
-AceCraftingProgress._ace_old_destroy = CraftingProgress.__user_destroy
-function AceCraftingProgress:destroy()
+AceCraftingProgress._ace_old__destroy_ingredient_leases = CraftingProgress._destroy_ingredient_leases
+function AceCraftingProgress:_destroy_ingredient_leases()
+   self:_ace_old__destroy_ingredient_leases()
    self:destroy_working_ingredient()
-   self:_ace_old_destroy()
 end
 
 function AceCraftingProgress:destroy_working_ingredient()
@@ -20,7 +20,6 @@ function AceCraftingProgress:create(order, crafter)
    local recipe = order:get_recipe()
    crafter = crafter or order:get_current_crafter()
    local crafter_component = crafter:get_component('stonehearth:crafter')
-   local workshop = crafter_component:get_current_workshop()
    self._sv.crafter_id = crafter:get_id()
 
    --The recipes may call for different effects (based on the workbench type)
@@ -42,8 +41,7 @@ function AceCraftingProgress:create(order, crafter)
       game_seconds = secs_per_work_unit * recipe.work_units
    end
 
-   local container_entity_data = radiant.entities.get_entity_data(workshop, 'stonehearth:table')
-   self._sv.working_ingredient_uri = recipe.working_ingredient or (container_entity_data and container_entity_data.working_ingredient)
+   self._sv.working_ingredient_uri = recipe.working_ingredient
 
    self._sv.workshop_modifier = 1
    self._sv.game_seconds_total_raw = game_seconds
@@ -71,25 +69,32 @@ end
 AceCraftingProgress._ace_old_crafting_started = CraftingProgress.crafting_started
 function AceCraftingProgress:crafting_started()
    -- if a working ingredient uri was specified, create that entity and replace the existing ingredients with it
-   if self._sv.working_ingredient_uri and not self._sv._working_ingredient then
+   if not self._sv._working_ingredient then
       local crafter = radiant.entities.get_entity(self._sv.crafter_id)
       local workshop = crafter and crafter:get_component('stonehearth:crafter'):get_current_workshop()
       local entity_container = workshop and workshop:get_component('entity_container')
 
       if entity_container then
-         -- first remove/hide all other ingredients from the workshop
-         for id, child in entity_container:each_child() do
-            child:get_component('render_info'):set_visible(false)
+         local uri = self._sv.working_ingredient_uri
+         if not uri then
+            local container_entity_data = radiant.entities.get_entity_data(workshop, 'stonehearth:table')
+            uri = container_entity_data and container_entity_data.working_ingredient
          end
+         if uri then
+            -- first remove/hide all other ingredients from the workshop
+            for id, child in entity_container:each_child() do
+               child:get_component('render_info'):set_visible(false)
+            end
 
-         local working_ingredient = radiant.entities.create_entity(self._sv.working_ingredient_uri, {owner = workshop})
-         local container_entity_data = radiant.entities.get_entity_data(workshop, 'stonehearth:table')
-         local offset = container_entity_data and container_entity_data.drop_offset and radiant.util.to_point3(container_entity_data.drop_offset)
-         radiant.entities.add_child(workshop, working_ingredient)
-         if offset then
-            working_ingredient:add_component('mob'):move_to(offset)
+            local working_ingredient = radiant.entities.create_entity(uri, {owner = workshop})
+            local container_entity_data = radiant.entities.get_entity_data(workshop, 'stonehearth:table')
+            local offset = container_entity_data and container_entity_data.drop_offset and radiant.util.to_point3(container_entity_data.drop_offset)
+            radiant.entities.add_child(workshop, working_ingredient)
+            if offset then
+               working_ingredient:add_component('mob'):move_to(offset)
+            end
+            self._sv._working_ingredient = working_ingredient
          end
-         self._sv._working_ingredient = working_ingredient
       end
    end
 
