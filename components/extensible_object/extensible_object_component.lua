deb53af7e57d7f9e85d271633b09143de21a9736
DaniAngione <daniangi@gmail.com>
2023-09-22 23:34:46 +0200

    Still WIP/Broken

--- components/extensible_object/extensible_object_component.lua
+++ components/extensible_object/extensible_object_component.lua
@@ -5,16 +5,23 @@ local log = radiant.log.create_logger('extensible_object')
 
 local ExtensibleObjectComponent = class()
 
+function ExtensibleObjectComponent:initialize()
+   self._json = radiant.entities.get_json(self)
+end
+
 function ExtensibleObjectComponent:create()
-	local json = radiant.entities.get_json(self)
-	self._sv.extend_command = json.extend_command or 'stonehearth_ace:commands:toggle_enabled_on'
-	self._sv.remove_command = json.remove_command or 'stonehearth_ace:commands:toggle_enabled_off'
-	self._sv.extension_entity = json.extension_entity
-	self._sv.render_model = json.render_model
-	self.__saved_variables:mark_changed()
+	self._is_create = true
+end
+
+function ExtensibleObjectComponent:restore()
 end
 
 function ExtensibleObjectComponent:activate()
+	self._sv.extend_command = self._json.extend_command or 'stonehearth_ace:commands:extensible_object:extend'
+	self._sv.remove_command = self._json.remove_command or 'stonehearth_ace:commands:extensible_object:remove'
+	self._sv.extension_entity = self._json.extension_entity
+	self._sv.render_model = self._json.render_model
+	self.__saved_variables:mark_changed()
    self._rotations = radiant.util.get_rotations_table(self._json)
 end
 
