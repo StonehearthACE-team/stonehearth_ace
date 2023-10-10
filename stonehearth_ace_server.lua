09a29f7ba9bf415206b0e64f9111fdcb1ad2657e
paulthegreat <paulthegreat@users.noreply.github.com>
2023-10-10 02:31:20 -0500

    fix to sound service typo
    also untested changes to find_target_observer for pets and pet owners (an attacked pet should trigger an out-of-combat owner to defend their pet; vice-versa; and a pet owner attacking a target should raise that target's aggro level to the highest for their pets)

--- stonehearth_ace_server.lua
+++ stonehearth_ace_server.lua
@@ -148,6 +148,7 @@ local monkey_patches = {
    ace_periodic_health_modification = 'stonehearth.data.buffs.scripts.periodic_health_modification',
    ace_personality_component = 'stonehearth.components.personality.personality_component',
    ace_pet_component = 'stonehearth.components.pet.pet_component',
+   ace_pet_owner_component = 'stonehearth.components.pet.pet_owner_component',
    ace_pickup_item_type_from_backpack_action = 'stonehearth.ai.actions.pickup_item_type_from_backpack_action',
    ace_pickup_placed_item_adjacent_action = 'stonehearth.ai.actions.pickup_placed_item_adjacent_action',
    ace_pillage_mission = 'stonehearth.services.server.game_master.controllers.missions.pillage_mission',
