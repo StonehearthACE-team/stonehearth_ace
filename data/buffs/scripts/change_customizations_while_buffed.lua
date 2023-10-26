8f9b23b376681b176e90feadf902ec1206043202
paulthegreat <paulthegreat@users.noreply.github.com>
2023-10-26 14:06:48 -0500

    tweaks to customization code

--- data/buffs/scripts/change_customizations_while_buffed.lua
+++ data/buffs/scripts/change_customizations_while_buffed.lua
@@ -2,15 +2,19 @@ local ChangeCustomizations = class()
 
 function ChangeCustomizations:on_buff_added(entity, buff)
    local script_info = buff:get_json().script_info
-   for subcategory, style in pairs(script_info.customizations) do
-      entity:add_component('stonehearth:customization'):change_customization(subcategory, style, true)
+   if script_info.customizations then
+      for subcategory, style in pairs(script_info.customizations) do
+         entity:add_component('stonehearth:customization'):change_customization(subcategory, style, true)
+      end
    end
 end
 
 function ChangeCustomizations:on_buff_removed(entity, buff)
    local script_info = buff:get_json().script_info
-   for subcategory, style in pairs(script_info.customizations) do
-      entity:get_component('stonehearth:customization'):restore_cached_customization(subcategory)
+   if script_info.customizations then
+      for subcategory, style in pairs(script_info.customizations) do
+         entity:get_component('stonehearth:customization'):restore_cached_customization(subcategory)
+      end
    end
 end
 
