c26a30ff53d183852f5b366add68782d90163598
paulthegreat <paulthegreat@users.noreply.github.com>
2024-03-03 01:59:06 -0600

    added "subcontainer" option for drink containers

--- ai/actions/drinking/get_drink_from_container_adjacent.lua
+++ ai/actions/drinking/get_drink_from_container_adjacent.lua
@@ -26,11 +26,6 @@ function GetDrinkFromContainerAdjacent:run(ai, entity, args)
       return
    end
 
-   local model_variant
-   if container_data.dynamic_serving_model then
-      model_variant = container:add_component('render_info'):get_model_variant()
-   end
-
    local quality_component = container:get_component("stonehearth:item_quality")
    local container_quality = (quality_component and quality_component:get_quality()) or 0
 
@@ -45,6 +40,37 @@ function GetDrinkFromContainerAdjacent:run(ai, entity, args)
 
    -- go ahead and release it for others while we sit and drink
    stonehearth.ai:release_ai_lease(container, entity)
+
+   -- if specified, create a subcontainer entity to drop on the ground and drink from that instead
+   -- while loop allows for nesting dolls, I guess (primary functionality is water buckets from wells)
+   while container_data.subcontainer do
+      local stacks_per_serving = container_data.stacks_per_serving or 1
+      if stacks_per_serving > 0 then
+         ai:unprotect_argument(container)
+         if not radiant.entities.consume_stack(container, stacks_per_serving) then
+            ai:abort('Cannot drink: Drink container is empty.')
+            return
+         end
+      end
+
+      local subcontainer = radiant.entities.create_entity(container_data.subcontainer, { owner = entity })
+      if container_quality > stonehearth.constants.item_quality.NORMAL then
+         subcontainer:add_component('stonehearth:item_quality'):initialize_quality(container_quality, nil, nil, {override_allow_variable_quality=true})
+      end
+
+      stonehearth.ai:pickup_item(ai, entity, subcontainer)
+      ai:execute('stonehearth:drop_carrying_now')
+      container = subcontainer
+      container_data = radiant.entities.get_entity_data(container, 'stonehearth_ace:drink_container')
+      radiant.entities.turn_to_face(entity, container)
+
+      -- no effects for subcontainers; do we really need them?
+   end
+
+   local model_variant
+   if container_data.dynamic_serving_model then
+      model_variant = container:add_component('render_info'):get_model_variant()
+   end
 
    local stacks_per_serving = container_data.stacks_per_serving or 1
    if stacks_per_serving > 0 then
