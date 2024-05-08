4d9de7ab6a47b5dfcae1bb1cd1b549d492bf16da
paulthegreat <paulthegreat@users.noreply.github.com>
2024-05-07 20:02:34 -0500

    added recipe unlock bulletin

--- entities/consumables/scripts/unlock_random_recipes.lua
+++ entities/consumables/scripts/unlock_random_recipes.lua
@@ -16,6 +16,8 @@
 ]]
 
 local rng = _radiant.math.get_default_rng()
+local job_lib = require 'stonehearth_ace.lib.job.job_lib'
+
 local UnlockRandomRecipes = class()
 
 local log = radiant.log.create_logger('unlock_random_recipes_consumable_script')
@@ -43,7 +45,7 @@ function UnlockRandomRecipes.use(consumable, consumable_data, player_id, target_
          local unlocked = job_info:get_manually_unlocked()
          for _, recipe in ipairs(recipes) do
             if not unlocked[recipe] then
-               table.insert(possible_recipes, {job_info = job_info, recipe = recipe})
+               table.insert(possible_recipes, {job = job, recipe = recipe})
             end
          end
       end
@@ -54,29 +56,24 @@ function UnlockRandomRecipes.use(consumable, consumable_data, player_id, target_
       return false
    end
 
-   local jobs = {}
+   local recipes_by_job = {}
 
    while next(possible_recipes) and num_to_unlock > 0 do
       num_to_unlock = num_to_unlock - 1
       local index = rng:get_int(1, #possible_recipes)
       local selection = table.remove(possible_recipes, index)
-      local job_info = selection.job_info
-      jobs[job_info] = (jobs[job_info] or 0) + 1
-
-      job_info:manually_unlock_recipe(selection.recipe)
-      log:debug('unlocking %s recipe "%s"', job_info:get_alias(), selection.recipe)
-   end
-
-   if consumable_data.show_bulletin ~= false then
-      local bulletin_title = consumable_data.bulletin_title or 'stonehearth_ace:data.commands.use_recipe.bulletin_title'
-      for job_info, count in pairs(jobs) do
-         stonehearth.bulletin_board:post_bulletin(player_id)
-               :set_sticky(true)
-               :set_data({title = consumable_data.bulletin_title})
-               :add_i18n_data('job_name', job_info:get_class_name())
-               :add_i18n_data('recipe_count', count)
+      local job = selection.job
+      local recipes = recipes_by_job[job]
+      if not recipes then
+         recipes = {}
+         recipes_by_job[job] = recipes
       end
+      table.insert(recipes, selection.recipe)
+      log:debug('selected recipe "%s" for job "%s" to unlock', selection.recipe, job)
    end
+
+   local bulletin_titles = consumable_data.bulletin_title and {consumable_data.bulletin_title}
+   job_lib.unlock_recipes(player_id, recipes_by_job, bulletin_titles, consumable_data.show_bulletin ~= false)
 
    return true
 end
--- monkey_patches/ace_unlock_recipe_encounter.lua
+++ monkey_patches/ace_unlock_recipe_encounter.lua
@@ -1,7 +1,20 @@
 local UnlockRecipeEncounter = require 'stonehearth.services.server.game_master.controllers.encounters.unlock_recipe_encounter'
 local game_master_lib = require 'lib.game_master.game_master_lib'
+local job_lib = require 'stonehearth_ace.lib.job.job_lib'
 
 local AceUnlockRecipeEncounter = class()
+
+AceUnlockRecipeEncounter._ace_old__destroy = UnlockRecipeEncounter.__user_destroy
+function AceUnlockRecipeEncounter:destroy()
+   self:_ace_old__destroy()
+
+   if self._sv._recipe_bulletins then
+      for _, bulletin in ipairs(self._sv._recipe_bulletins) do
+         stonehearth.bulletin_board:remove_bulletin(bulletin)
+      end
+      self._sv._recipe_bulletins = nil
+   end
+end
 
 function AceUnlockRecipeEncounter:start(ctx, info)
    assert(info.job and info.recipe_key)
@@ -10,20 +23,8 @@ function AceUnlockRecipeEncounter:start(ctx, info)
 
    local job_info = stonehearth.job:get_job_info(ctx.player_id, info.job)
    -- unlock the recipe(s)
-   self:_unlock_recipes(job_info, info.recipe_key)
-
-   --create a bulletin about it!
-   if info.bulletin_title then
-      local bulletin_data = {
-         title = info.bulletin_title,
-         notification_closed_callback = '_on_closed'
-      }
-
-      self._sv.recipe_bulletin = stonehearth.bulletin_board:post_bulletin(ctx.player_id)
-            :set_callback_instance(self)
-            :set_sticky(true)
-            :set_data(bulletin_data)
-   end
+   self._sv._recipe_bulletins = job_lib.unlock_recipes(ctx.player_id, {[info.job] = {info.recipe_key}}, info.bulletin_title, not info.bulletin_title, self)
+   --self:_unlock_recipes(job_info, info.recipe_key)
 
    ctx.arc:trigger_next_encounter(ctx)
 end
