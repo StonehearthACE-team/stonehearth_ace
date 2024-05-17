8cedabc914aecba61c7e2305012b15a196faddc2
DaniAngione <daniangi@gmail.com>
2024-05-17 15:48:11 -0300

    Fix to turrets being repaired without need
    Fix to quality crops for RC and NA

--- monkey_patches/ace_na_guildmaster_town_bonus.lua
+++ monkey_patches/ace_na_guildmaster_town_bonus.lua
@@ -5,6 +5,22 @@ ACE_RECIPES_TO_UNLOCK = radiant.resources.load_json('stonehearth_ace:data:recipe
 
 local RECIPE_UNLOCK_BULLETIN_TITLES = {}
 
+function AceGuildmasterTownBonus:get_adjusted_item_quality_chances()
+   -- Replaces constants.crafting.ITEM_QUALITY_CHANCES when the bonus is in effect.
+   return {
+      {{1, 1}},
+      {{1, 1}},
+      {{1, 0.95}, {2, 0.05}},
+      {{1, 0.9}, {2, 0.10}},
+      {{1, 0.85}, {2, 0.07}, {3, 0.06}, {4, 0.02}},
+      {{1, 0.80}, {2, 0.08}, {3, 0.07}, {4, 0.05}}
+   }
+end
+
+function AceGuildmasterTownBonus:get_max_crafting_quality()
+   return stonehearth.constants.item_quality.MASTERWORK or 4
+end
+
 function AceGuildmasterTownBonus:get_recipe_unlocks()
    return ACE_RECIPES_TO_UNLOCK, RECIPE_UNLOCK_BULLETIN_TITLES
 end
