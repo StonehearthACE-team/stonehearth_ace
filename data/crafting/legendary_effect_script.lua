178e728701f60f9998afae6b96aea8cb1e99bbd9
DaniAngione <daniangi@gmail.com>
2024-08-06 13:14:42 -0300

    Fix/Improvement to legendary crafting celebrations

---
+++ data/crafting/legendary_effect_script.lua
@@ -0,0 +1,32 @@
+local legendary_effect = {}
+
+function legendary_effect.post_craft(ai, crafter, workshop, recipe, all_products)
+   if ai then
+      if crafter and recipe and stonehearth.client_state:get_client_gameplay_setting(crafter:get_player_id(), 'stonehearth_ace', 'show_legendary_craft_notification', true) then
+         legendary_effect._show_bulletin(crafter, recipe)
+      end
+
+      ai:execute('stonehearth:run_effect', { effect = 'emote_confetti' })
+      ai:execute('stonehearth:run_effect', { effect = 'emote_delighted' })
+   end
+end
+
+function legendary_effect._show_bulletin(crafter, recipe)
+   if not crafter or not recipe then
+      return
+   end
+
+   local player_id = crafter:get_player_id()
+   local bulletin = stonehearth.bulletin_board:post_bulletin(player_id)
+      :set_data({
+         title = 'i18n(stonehearth_ace:ui.game.bulletin.legendary_craft_bulletin.title)',
+         zoom_to_entity = crafter
+      })
+      :set_active_duration('3h')
+      :add_i18n_data('crafter', crafter)
+      :add_i18n_data('recipe', recipe.recipe_name)
+
+   return bulletin
+end
+
+return legendary_effect
