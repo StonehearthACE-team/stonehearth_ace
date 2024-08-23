672409bdfcab1d722b5164720541ce5de4c9a93f
DaniAngione <daniangi@gmail.com>
2024-08-23 15:34:13 -0300

    Fix to donation dialog quests getting stuck if you never accept before timer expires

--- monkey_patches/ace_bulletin.lua
+++ monkey_patches/ace_bulletin.lua
@@ -5,4 +5,12 @@ function AceBulletin:get_data()
    return self._sv.data
 end
 
+AceBulletin._ace_old__on_remove_bulletin_timer = Bulletin._on_remove_bulletin_timer
+function AceBulletin:_on_remove_bulletin_timer()
+   -- (ACE) Triggering an event for the encounters that depend on bulletins to clean up appropriately when it occurs
+   radiant.events.trigger_async(self, 'stonehearth:bulletin:on_remove_bulletin_timer')
+
+   self:_ace_old__on_remove_bulletin_timer()
+end
+
 return AceBulletin
