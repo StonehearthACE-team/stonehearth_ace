e78e9e6ebf8652e050750c51e816b2cac3e337f1
DaniAngione <daniangi@gmail.com>
2025-06-25 20:49:32 +0200

    Release 0.9.6.19c

--- monkey_patches/ace_sleepiness_observer.lua
+++ monkey_patches/ace_sleepiness_observer.lua
@@ -53,4 +53,46 @@ function AceSleepinessObserver:finish_sleeping()
 	radiant.entities.remove_thought(self._entity, 'stonehearth:thoughts:sleepiness:bad_dream')
 end
 
+function AceSleepinessObserver:_add_finished_sleeping_thoughts(bed, slept_outside, has_roommate)
+   if bed then
+      local bed_data = radiant.entities.get_entity_data(bed, 'stonehearth:bed')
+	  local ownable_component = bed:get_component('stonehearth:ownable_object')
+	  local traits_component = self._entity:get_component('stonehearth:traits')
+      if bed_data and bed_data.shelter.score > stonehearth.constants.score.shelter.COMFY_BED_THRESHOLD then
+         radiant.entities.add_thought(self._entity, 'stonehearth:thoughts:sleeping:slept_in_comfy_bed')
+      end
+
+	  -- ACE: Adding support for barracks' bed
+      local is_owner = self:_owns_bed(bed)
+      if not is_owner then
+		if ownable_component and ownable_component:get_reservation_type() == stonehearth.constants.combat.MILITARY_OWNERSHIP_TYPE then
+         	radiant.entities.add_thought(self._entity, 'stonehearth_ace:thoughts:sleeping:slept_in_shared_bed_military')
+		else
+			radiant.entities.add_thought(self._entity, 'stonehearth:thoughts:sleeping:slept_in_shared_bed')
+		end
+      end
+
+      -- Switching the thought to a positive one as a result of this thread: https://discourse.stonehearth.net/t/slept-in-a-shared-room-shouldnt-be-a-thing/31368/25
+	  -- ACE: Adding it for Hearthlings with the 'Loner' trait
+	  local loner = traits_component and traits_component:has_trait('stonehearth:traits:loner')
+      if has_roommate and loner then
+         radiant.entities.add_thought(self._entity, 'stonehearth:thoughts:sleeping:slept_in_shared_room')
+      end
+
+      if is_owner and not slept_outside and not has_roommate then
+         radiant.entities.add_thought(self._entity, 'stonehearth:thoughts:sleeping:well_rested')
+      end
+
+      if slept_outside then
+         -- We don't think this when sleeping outside on the ground.
+         -- We let slept_on_ground takes care of that since the player currently has no ability
+         -- to control whether hearthlings sleep inside or outside on the ground.
+		 -- ACE: Add a "Move to shelter" before they fall asleep on the ground so we can have both debuffs? >:) It would also fix some funny things like lings sleeping in the water or on construction sites since they'd try going somewhere covered...
+         radiant.entities.add_thought(self._entity, 'stonehearth:thoughts:sleeping:slept_outside')
+      end
+   else
+      radiant.entities.add_thought(self._entity, 'stonehearth:thoughts:sleeping:slept_on_ground')
+   end
+end
+
 return AceSleepinessObserver
